20e1c7f286697f16a6310a191c9029a8a5eb67c7
ant <ant@theantsrobotics.org>
2026-02-11 14:09:17 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from cx_Freeze import setup
 
 
 build_exe_options = {
-    "include_files": ["data"],
-    "excludes": ["tkinter", "unittest"],
+    'include_files': ['data'],
+    'excludes': ['tkinter', 'unittest'],
 }
 
 
 setup(
-    name="FTC Field",
-    version="0.1.0",
-    description="FTC Field Path Planner",
-    options={"build_exe": build_exe_options},
-    executables=[{"script": "main.py", "base": "gui"}],
+    name='Coyote',
+    version='0.1.0',
+    description='Path generator for FTC Robotics',
+    options={'build_exe': build_exe_options},
+    executables=[{'script': 'main.py', 'base': 'gui'}],
 )
 
